1ee81935bf4e321dbd8db5a578c012067e0f649c
DhanushS-2005 <dhanushdhanush1357@gmail.com>
2025-07-22 22:12:35 +0530

    second commit

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,31 +1,56 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
-
   let [note, setNote] = useState({
-    title : "",
-    content : ""
-  })
+    title: "",
+    content: "",
+  });
+
+  let [isExpanded, setIsExpanded] = useState(false);
 
   function handleChange(event) {
-    let {name, value} = event.target;
+    let { name, value } = event.target;
 
-    setNote(prev => ({...prev, [name] : value}));
+    setNote((prev) => ({ ...prev, [name]: value }));
   }
 
-  function submit(event){
+  function submit(event) {
     props.onAdd(note);
-    setNote({title: "", content: ""});
+    setNote({ title: "", content: "" });
     event.preventDefault();
   }
 
+  function expand(){
+    setIsExpanded(true);
+  }
 
   return (
     <div>
-      <form>
-        <input value = { note.title } onChange={handleChange} name="title" placeholder="Title" />
-        <textarea value = { note.content } onChange={handleChange} name="content" placeholder="Take a note..." rows="3" />
-        <button onClick={submit}>Add</button>
+      <form className="create-note">
+        {isExpanded && (
+          <input
+          value={note.title}
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+        />
+        )}
+        <textarea
+          value={note.content}
+          onClick={expand}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submit}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Note(props) {
-
-  function deleteItem(){
-    props.onDelete(props.id)
+  function deleteItem() {
+    props.onDelete(props.id);
   }
 
-
-
   return (
-    <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button onClick={deleteItem} >DELETE</button>
+    <div className="notesContainer">
+      <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        <button onClick={deleteItem}>
+          <DeleteIcon />
+        </button>
+      </div>
     </div>
   );
 }
